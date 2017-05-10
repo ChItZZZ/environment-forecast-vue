4fe00b1a8add55edb55804f2dd0debbf1781794c
ChItZZZ <648267970@qq.com>
2017-05-10 23:45:45 +0800

    add kpicategory.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -46,7 +46,7 @@
                                 <div class="row">
                                     <div class="cell"></div>
                                     <div class="cell">
-                                        <button @click="ioc_login()" class="btn btn-default form-btn">登录</button>
+                                        <button @click="ioc_login" class="btn btn-default form-btn">登录</button>
                                     </div>
                                 </div>
                             </div>
@@ -61,6 +61,28 @@
     export default {
         name: "login",
         data(){
+
+        },
+        methods: {
+            ioc_login: function () {
+                $scope.login_status = '';
+                var _n = $scope.login_name;
+                var _p = $scope.login_password;
+                if (_n == undefined || _n == "" || _p == undefined || _p == "") {
+                    $scope.login_status = '用户名/密码不能为空!';
+                }
+                tokenFactory.login({
+                    'X-Username': _n,
+                    'X-Password': encryptPassword(_p)
+                }).post({},
+                    function success(data, headers) {
+                        sessionService.saveToken(data.data, headers()['x-auth-token']);
+                    },
+                    function error(data) {
+                        inform.add('账号或密码错误,请重新登录.', error_conf);
+                        //inform.add('网络错误,请稍后重新登录.', error_conf);
+                    });
+            }
 
         }
     }
